74036f37679c528e9748cbe699a8309f251a6a4d
amitbend <ajbendor@gmail.com>
2018-06-13 20:57:27 +0300

    lms script update

--- replace.sh
+++ replace.sh
@@ -1,1 +1,1 @@
-scp /home/amit/code/side/marvel/lms/  madrasateacher@52.233.184.92:/opt/bitnami/apps/edx/conf/lms.env.json
+scp $(pwd)/lms/lms.env.json madrasateacher@52.233.184.92:/opt/bitnami/apps/edx/conf
